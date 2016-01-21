d89771a55e8bce7f35f96010a615dbec8c9c8e49
jamierose <jrose111284@gmail.com>
2016-01-21 20:56:02 +0000

    home controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#show' 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
